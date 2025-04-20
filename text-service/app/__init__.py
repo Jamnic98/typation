a420384480e587bbb4fe34c6cdfebeadcfa01921
Jamnic98 <jamiestimpson30@gmail.com>
2025-04-20 22:12:11 +0100

    added dockerfiles and docker compose file for running app

---
+++ text-service/app/__init__.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def create_app():
+  app = FastAPI()
+  app.add_middleware(
+      CORSMiddleware,
+      allow_origins=["*"],
+      allow_credentials=True,
+      allow_methods=["*"],
+      allow_headers=["*"],
+  )
+
+  @app.get("/")
+  def read_root():
+    return {"Hello": "World"}
+
+  return app
