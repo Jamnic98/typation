f6c9ccd2d4d339ef08302cb19e1ea153f1ed9254
Jamie Stimpson <Jamnic98>
2025-05-29 06:00:12 +0100

    update

---
+++ client/src/utils/helpers.ts
@@ -0,0 +1,18 @@
+import { CursorStyles } from 'types/global'
+
+export const randomRotation = Math.floor(Math.random() * 201) - 100
+
+export const getCursorStyle = (cursorStyle: CursorStyles | undefined) => {
+  switch (cursorStyle) {
+    case CursorStyles.UNDERSCORE:
+      return 'animate-flash-underscore'
+    case CursorStyles.BLOCK:
+      return 'animate-flash-block'
+    case CursorStyles.OUTLINE:
+      return 'animate-flash-outline'
+    case CursorStyles.PIPE:
+      return 'animate-flash-pipe'
+    default:
+      return 'none'
+  }
+}
