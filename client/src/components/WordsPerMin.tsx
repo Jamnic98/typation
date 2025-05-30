f7ceb4f6857512367488cab54b2e680255b66ac3
Jamie Stimpson <Jamnic98>
2025-05-30 23:35:57 +0100

    test husky

--- client/src/components/WordsPerMin.tsx
+++ client/src/components/WordsPerMin.tsx
@@ -2,6 +2,6 @@ interface WordsPerMinProps {
   wpm: number
 }
 
-export const WordsPerMin = ({ wpm }: WordsPerMinProps) => {
-  return <div className="text-xl font-mono tracking-widest select-none">wpm:{wpm}</div>
-}
+export const WordsPerMin = ({ wpm }: WordsPerMinProps) => (
+  <div className="text-xl font-mono tracking-widest select-none">wpm:{wpm}</div>
+)
