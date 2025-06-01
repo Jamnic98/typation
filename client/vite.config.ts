696d289c02b3af433512b7ebd8de8770e112e201
Jamnic98 <jamiestimpson30@gmail.com>
2025-06-01 12:05:25 +0100

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-})
+  server: {
+    host: true,
+    ...(mode === 'development' && {
+      proxy: {
+        '/text': {
+          target: 'http://192.168.68.104:5000',
+          changeOrigin: true,
+        },
+      },
+      allowedHosts: ['192.168.68.104', 'ubuntu-server.local'],
+    }),
+  },
+}))
